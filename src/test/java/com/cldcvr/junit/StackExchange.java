e0b47ece3df562ef0b16a8b6a91c2806ad4c832a
dheerajkumar <iamuploadingpictures@gmail.com>
2020-05-02 16:00:01 +0530

    Adding scenario to demonstrate assertion and validations

--- src/test/java/com/cldcvr/junit/StackExchange.java
+++ src/test/java/com/cldcvr/junit/StackExchange.java
@@ -3,11 +3,14 @@ package com.cldcvr.junit;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ResponseBody;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 
@@ -38,6 +41,22 @@ public class StackExchange {
 	public void getBadgeWithWrongSortCode() {
 		SerenityRest.given().when().get("badges/tags?order=desc&sort=wrong&site=stackover").then().statusCode(400);
 
+	}
+	
+	@Test
+	public void getBadgeWithWrongMinMax() {
+		@SuppressWarnings("rawtypes")
+		ResponseBody rb = SerenityRest.get("badges?order=desc&min=1&max=010&sort=rank&site=stackoverflow");
+		JsonPath jsonPathEvaluator = rb.jsonPath();
+		String errorID = jsonPathEvaluator.get("error_id")+"";
+		String error_message = jsonPathEvaluator.get("error_message");
+		String error_name = jsonPathEvaluator.get("error_name");
+		
+		// Validation for the body contents
+		Assert.assertTrue(errorID.equalsIgnoreCase("400"));
+		Assert.assertTrue(error_message.equalsIgnoreCase("min"));
+		Assert.assertTrue(error_name.equalsIgnoreCase("bad_parameter"));
+		
 	}
 
 }
